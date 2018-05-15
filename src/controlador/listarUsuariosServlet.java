e085973ca34f8508a4a6dc23ab46e6ef2aeefba2
Yaiza <yaimsc@gmail.com>
2018-05-15 08:41:09 +0200

    servlet

---
+++ src/controlador/listarUsuariosServlet.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class listarUsuariosServlet extends HttpServlet{
+
+	public void doGet (HttpServletRequest request, HttpServletResponse response){
+		
+		
+	}
+}
